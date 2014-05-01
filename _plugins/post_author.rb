27172b5913aa45f1dedf5bce4d4b770e8a6c5e9b
Izhar Gavish <izhar@tikalk.com>
2014-05-01 17:50:23 +0300

    Fix author tag bug

--- _plugins/post_author.rb
+++ _plugins/post_author.rb
@@ -4,7 +4,8 @@ require 'pry'
 module Jekyll
   class PostAuthor < Liquid::Tag
     def render(context)
-      if author = context["page"]["author"] || context["post"]["author"]
+      page_or_post = context["page"] || context["post"] || {}
+      if author = page_or_post["author"]
         author_data = context["site"]["data"]["users"].detect{|data| data["login"] == author}
         author_data ? "<a href='#{author_data['permalink'] || ('/users/'+author)}'>#{author}</a>" : author
       else
