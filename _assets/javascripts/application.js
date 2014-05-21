7225ac7812f71ddcbc4e71f2d8bfd59e662b5a95
Izhar Gavish <izhar@tikalk.com>
2014-05-21 14:20:50 +0300

    very basic carousel for welcome page

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -15,4 +15,18 @@ $('document').ready(function() {
 
   // enable careers vertiavl carousel
   Drupal.behaviors.tikalScrollItems.attach();
+
+  if ($('.jcarousel').length > 0) {
+    var carousel = $('.jcarousel');
+    var slides = carousel.find('li');
+    var numberOfSlides = slides.length;
+    var num = 0;
+    window.setInterval(function () {
+        // increase by num 1, reset to 0 at 4
+        num = (num + 1) % (numberOfSlides);
+        var leftPosition = -(slides.first().width() * num) + "px"; 
+
+        carousel.find('ul').attr('style', 'left: '+leftPosition);
+    }, 4000); // repeat forever, polling every 3 seconds
+  }
 });
